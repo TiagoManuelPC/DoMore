44a52a9b309427bb1af76413d812b6137907fbb0
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2025-12-05 20:25:22 +0000

    backend WIP

--- dotnet/API/Controllers/BaseApiController.cs
+++ dotnet/API/Controllers/BaseApiController.cs
@@ -1,11 +1,15 @@
-using Microsoft.AspNetCore.Http;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseApiController : ControllerBase
+    public class BaseApiController() : ControllerBase
     {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator service is unavailable.");
     }
 }
